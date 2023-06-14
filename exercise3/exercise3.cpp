d737086691a2196a8df218b6b5cee93bff2ad4f7
bimbim1214 <bimoadityapangestu@gmail.com>
2023-06-14 10:22:02 +0700

    Add project files.

---
+++ exercise3/exercise3.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class pengarang;
+class penerbit {
+public:
+	string nama;
+	vector<pengarang*> daftar_pengarang;
+	penerbit(string pNama) :nama(pNama) {
+		cout << "penerbit \"" << nama << "\" ada\n";
+	}
+	~penerbit() {
+		cout << "penerbit \"" << nama << "\" tidak ada\n";
+	}
+	void tambahpengarang(pengarang*);
+	void cetakpengarang();
+	
+
+};
+
+class pengarang {
+public:
+	string nama;
+	vector<penerbit*> daftar_penerbit;
+	
+	pengarang(string pNama) :nama(pNama) {
+		cout << "pengarang \"" << nama << "\" ada\n";
+	}
+	~pengarang() {
+		cout << "pengarang \"" << nama << "\" tidak ada\n";
+	}
+	void tambahpenerbit(penerbit*);
+	void cetakpenerbit();
+};
+
+void penerbit::tambahpengarang(pengarang* pPengarang) {
+	daftar_pengarang.push_back(pPengarang);
+}
+void penerbit::cetakpengarang() {
+	cout << "daftar pengarang yang menaungi penerbit \"" << this->nama << "\":\n";
+	for (auto& a : daftar_pengarang) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+
+void pengarang::tambahpenerbit(penerbit* pPenerbit) {
+	daftar_penerbit.push_back(pPenerbit);
+	pPenerbit->tambahpengarang(this);
+}
+
+void pengarang::cetakpenerbit() {
+	cout << "daftar penerbit yang dikuti pengarang \"" << this->nama << "\":";
+	for (auto& a : daftar_penerbit) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+
+}
+
+class buku {
+public:
+	string nama;
+	vector<pengarang*> daftar_pengarang;
+
+	buku(string pNama) :nama(pNama) {
+		cout << "pengarang \"" << nama << "\" ada\n";
+	}
+	~buku() {
+		cout << "pengarang \"" << nama << "\" tidak ada\n";
+	}
+}
+void buku::tambahpengarang(pengarang* pPengarang) {
+	daftar_pengarang.push_back(pPengarang);
+	pPengarang->tambahpengarang(this);
+
+	void tambahpengarang(pengarang*);
+	void cetakbuku();
+};
+
+int main() {
+	pengarang* verpengarang = new pengarang("joko");
+	pengarang* verpengarang = new pengarang("lia");
+	pengarang* verpengarang = new pengarang("giga");
+	pengarang* verpengarang = new pengarang("asroni");
+	pengarang* verpengarang = new pengarang("giga");
+	penerbit* verpenerbit1 = new penerbit("Gama Press");
+	penerbit* verpenerbit2 = new penerbit("Intan Pariwara");
+
+	varpengarang->tambahpenerbit(varpenerbit1);
+
+}
